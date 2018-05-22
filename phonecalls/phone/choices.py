9548155a89725439c44feb0727affc60c01e4bb1
Icaro Soares <icarosoares01@gmail.com>
2018-05-22 19:59:35 -0300

    Create phone app model

---
+++ phonecalls/phone/choices.py
@@ -0,0 +1,6 @@
+
+START = "start"
+END = "end"
+
+TYPE_CALL_CHOICES = ((START, "Start"),
+                     (END, "End"),)
--- phonecalls/phone/models.py
+++ phonecalls/phone/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from .choices import TYPE_CALL_CHOICES
+
 # Create your models here.
+
+
+class Phone(models.Model):
+    number = models.CharField(max_length=9)
+
+
+class Call(models.Model):
+    call_id = models.AutoField(primary_key=True)
+    source = models.ForeignKey(Phone, related_name="source",
+                               on_delete=models.PROTECT)
+    destination = models.ForeignKey(Phone, related_name="destination",
+                                    on_delete=models.PROTECT)
+
+
+class CallDetail(models.Model):
+    call_id = models.ForeignKey(Call, on_delete=models.PROTECT)
+    type_call = models.CharField(max_length=10, choices=TYPE_CALL_CHOICES)
+    timestamp = models.DateTimeField()
