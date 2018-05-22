c661b84c373fae5a9f8f3cc763ddec97fc4a2275
Icaro Soares <icarosoares01@gmail.com>
2018-05-22 20:23:23 -0300

    Add BillCall model

--- phonecalls/bill/models.py
+++ phonecalls/bill/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from phone.models import Call
+
 # Create your models here.
+
+
+class BillCall(models.Model):
+    call = models.ForeignKey(Call, on_delete=models.PROTECT)
+    call_start_date = models.DateTimeField()
+    call_start_time = models.TimeField()
+    call_duration = models.TimeField()
+    call_price = models.DecimalField(max_digits=10, decimal_places=2)
