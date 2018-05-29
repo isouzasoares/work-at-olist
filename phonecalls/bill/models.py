7f0467e163ddc07b63dfada8bce5ffea87eaca4e
Icaro Soares <icarosoares01@gmail.com>
2018-05-28 21:47:37 -0300

    Add unique together in model, create tests

--- phonecalls/bill/models.py
+++ phonecalls/bill/models.py
@@ -6,7 +6,7 @@ from phone.models import Call
 
 
 class BillCall(models.Model):
-    call = models.ForeignKey(Call, on_delete=models.PROTECT)
+    call_id = models.OneToOneField(Call, on_delete=models.PROTECT)
     call_start_date = models.DateField()
     call_start_time = models.TimeField()
     call_duration = models.DurationField()
--- phonecalls/phone/tests/test_models.py
+++ phonecalls/phone/tests/test_models.py
@@ -1,1 +1,36 @@
 from django.test import TestCase
+from django.db import IntegrityError
+from django.utils import timezone
+
+from phone.models import Phone, Call, CallDetail
+from phone.choices import START, END
+
+
+class CallModelTest(TestCase):
+
+    def setUp(self):
+        phone = Phone.objects.create(number="31985853903")
+        phone2 = Phone.objects.create(number="31985853901")
+        Call.objects.create(source=phone,
+                            destination=phone2,
+                            call_id=70)
+
+    def test_call_detail_add(self):
+        call = Call.objects.get(call_id=70)
+        start = timezone.datetime(2017, 5, 23, 21, 57, 13,
+                                  tzinfo=timezone.get_current_timezone())
+        CallDetail.objects.create(call_id=call,
+                                  type_call=START,
+                                  timestamp=start)
+        self.assertEqual(CallDetail.objects.count(), 1)
+        end = timezone.datetime(2017, 5, 23, 22, 00, 00,
+                                tzinfo=timezone.get_current_timezone())
+        CallDetail.objects.create(call_id=call,
+                                  type_call=END,
+                                  timestamp=end)
+        self.assertEqual(CallDetail.objects.count(), 2)
+
+        with self.assertRaises(IntegrityError):
+            CallDetail.objects.create(call_id=call,
+                                      type_call=START,
+                                      timestamp=end)
