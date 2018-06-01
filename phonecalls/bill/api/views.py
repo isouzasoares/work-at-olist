9ef0cdb73f90acd989526671098abad0e911ea3a
Icaro Soares <icarosoares01@gmail.com>
2018-05-31 22:54:02 -0300

    Added new filter to api call detail
    Added new item to requirement dateutil

--- phonecalls/bill/api/views.py
+++ phonecalls/bill/api/views.py
@@ -1,15 +1,48 @@
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
+
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from bill.models import BillCall
 from bill.api.serializers import BillCallSerializer
+from phone.choices import END
 
 
 class BillDetailList(ListAPIView):
     queryset = BillCall.objects.all()
     serializer_class = BillCallSerializer
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-        qs = qs.filter(
-            call_id__source__number=self.kwargs["source_number"])
-        return qs
+    def list(self, request, *args, **kwargs):
+        now = timezone.now().replace(day=1).date()
+        month_year = request.GET.get("month_year", None)
+        queryset = self.filter_queryset(self.get_queryset())
+
+        if not month_year:
+            month_year = now - relativedelta(months=1)
+        else:
+            try:
+                month_year = timezone.datetime.strptime(month_year, "%m/%Y")
+                month_year = month_year.date()
+            except ValueError:
+                return Response({"Date format is m/Y"},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+        month_year = month_year.replace(day=1)
+        if month_year < now:
+            queryset = queryset.filter(
+                call_id__source__number=kwargs["source_number"],
+                call_id__calldetail__type_call=END,
+                call_id__calldetail__timestamp__month=month_year.month,
+                call_id__calldetail__timestamp__year=month_year.year)
+            page = self.paginate_queryset(queryset)
+
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
+
+        return Response([])
--- phonecalls/bill/tests/test_bill_api.py
+++ phonecalls/bill/tests/test_bill_api.py
@@ -1,4 +1,5 @@
 from datetime import time, timedelta
+from dateutil.relativedelta import relativedelta
 
 from django.test import TestCase
 from django.utils import timezone
@@ -6,7 +7,8 @@ from django.urls import reverse
 from django.urls.exceptions import NoReverseMatch
 
 from rest_framework.test import APIRequestFactory
-from phone.models import Call, Phone
+from phone.models import Call, Phone, CallDetail
+from phone.choices import END
 from bill.models import BillCall
 
 
@@ -14,43 +16,52 @@ class BillDetailApiTestCase(TestCase):
     client = APIRequestFactory()
 
     def setUp(self):
+        now = timezone.now()
+        before = now - relativedelta(months=1)
         phone = Phone.objects.create(number="31985853903")
         phone2 = Phone.objects.create(number="31985853901")
         call = Call.objects.create(source=phone,
                                    destination=phone2,
                                    call_id=70)
-        date_test = timezone.datetime(2017, 5, 23, 22, 00, 00,
-                                      tzinfo=timezone.get_current_timezone())
         BillCall.objects.create(call_id=call,
-                                call_start_date=date_test.date(),
+                                call_start_date=before.date(),
                                 call_start_time=time(22, 00),
                                 call_duration=timedelta(hours=0,
                                                         minutes=10,
                                                         seconds=0),
                                 call_price=1.26)
+        CallDetail.objects.create(call_id=call,
+                                  type_call=END,
+                                  timestamp=before)
+
         call2 = Call.objects.create(source=phone,
                                     destination=phone2,
                                     call_id=71)
-        date_test = timezone.datetime(2017, 5, 25, 10, 15, 20,
-                                      tzinfo=timezone.get_current_timezone())
+
         BillCall.objects.create(call_id=call2,
-                                call_start_date=date_test.date(),
+                                call_start_date=before.date(),
                                 call_start_time=time(10, 15, 20),
                                 call_duration=timedelta(hours=35,
                                                         minutes=10,
                                                         seconds=25),
                                 call_price=300.50)
+        CallDetail.objects.create(call_id=call2,
+                                  type_call=END,
+                                  timestamp=before)
+
         call3 = Call.objects.create(source=phone2,
                                     destination=phone,
                                     call_id=72)
-
         BillCall.objects.create(call_id=call3,
-                                call_start_date=date_test.date(),
+                                call_start_date=now.date(),
                                 call_start_time=time(10, 15, 20),
                                 call_duration=timedelta(hours=35,
                                                         minutes=10,
                                                         seconds=25),
                                 call_price=300.50)
+        CallDetail.objects.create(call_id=call3,
+                                  type_call=END,
+                                  timestamp=now)
 
     def test_detail_url(self):
         with self.assertRaises(NoReverseMatch):
@@ -61,16 +72,18 @@ class BillDetailApiTestCase(TestCase):
                                                        "31985853903"})
         self.assertEqual(self.client.get(url).status_code, 200)
 
-    def test_detail_items(self):
+    def test_detail_items_now(self):
+        now = timezone.now()
+        before = now - relativedelta(months=1)
         url = reverse("bill:bill_detail_list", kwargs={"source_number":
                                                        "31985853903"})
         data = [{'call_id': 70,
-                 'call_start_date': '2017-05-23',
+                 'call_start_date': before.strftime("%Y-%m-%d"),
                  'call_start_time': '22:00:00',
                  'call_price': 'R$ 1,26',
                  'call_duration': '0h10m00s'},
                 {'call_id': 71,
-                 'call_start_date': '2017-05-25',
+                 'call_start_date': before.strftime("%Y-%m-%d"),
                  'call_start_time': '10:15:20',
                  'call_price': 'R$ 300,50',
                  'call_duration': '35h10m25s'}]
@@ -81,3 +94,37 @@ class BillDetailApiTestCase(TestCase):
                                                        "31988888888"})
         items = self.client.get(url).json()
         self.assertEqual([], items)
+
+    def test_filter_month_year_get(self):
+        now = timezone.now().date()
+        now_str = now.strftime("%m/%Y")
+        before = now - relativedelta(months=1)
+        before_str = before.strftime("%m/%Y")
+
+        url = reverse("bill:bill_detail_list", kwargs={"source_number":
+                                                       "31985853903"})
+        get_url = "%s?month_year=%s" % (url, before_str)
+        items = self.client.get(get_url).json()
+        data = [{'call_id': 70,
+                 'call_start_date': before.strftime("%Y-%m-%d"),
+                 'call_start_time': '22:00:00',
+                 'call_price': 'R$ 1,26',
+                 'call_duration': '0h10m00s'},
+                {'call_id': 71,
+                 'call_start_date': before.strftime("%Y-%m-%d"),
+                 'call_start_time': '10:15:20',
+                 'call_price': 'R$ 300,50',
+                 'call_duration': '35h10m25s'}]
+        self.assertEqual(data, items)
+
+        get_url = "%s?month_year=%s" % (url, now_str)
+        items = self.client.get(get_url).json()
+        self.assertEqual([], items)
+
+    def test_date_error(self):
+        url = reverse("bill:bill_detail_list", kwargs={"source_number":
+                                                       "31985853903"})
+        get_url = "%s?month_year=%s" % (url, "2015-01")
+        items = self.client.get(get_url).json()
+        self.assertEqual(["Date format is m/Y"], items)
+        self.assertEqual(self.client.get(get_url).status_code, 400)
